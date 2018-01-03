ca685d1a7ac7a7d7ce98621aa7f2573ff9b835dd
bava1 <vinus133@yahoo.com>
2018-01-03 17:56:29 +0100

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,78 +1,21 @@
 
-window.onload = function(){
-	document.querySelector(".menu-but1").onclick = function(){ geAjaxStart() };		
-	document.querySelector(".menu-but2").onclick = function(){ geAjax1() };	
-	document.querySelector(".menu-but3").onclick = function(){ geAjax2() };	
-	document.querySelector(".menu-but4").onclick = function(){ geAjax3() };	
-	document.querySelector(".menu-but5").onclick = function(){ geAjax4() };	
-	document.querySelector(".menu-but6").onclick = function(){ geAjax5() };	
+window.onload = function(){	
+	document.querySelector(".menu-but2").onclick = function(){ getAjax("test.php") };	
+	document.querySelector(".menu-but2").onclick = function(){ getAjax("stat1.php") };	
+	document.querySelector(".menu-but2").onclick = function(){ getAjax("stat2.php") };	
+	document.querySelector(".menu-but2").onclick = function(){ getAjax("stat3.php") };	
+	document.querySelector(".menu-but2").onclick = function(){ getAjax("stat4.php") };	
+	document.querySelector(".menu-but2").onclick = function(){ getAjax("stat5.php") };	
+	document.querySelector(".sites").onclick = function(){ location.reload() };	
 }
 
-document.querySelector(".sites").onclick = function(){ location.reload() };	
-
-function geAjaxStart(){
+function getAjax(res){
 	var request = new XMLHttpRequest();	
 	request.onreadystatechange = function (){
 		if(request.readyState == 4 && request.status == 200){
 			document.querySelector(".third").innerHTML = request.responseText;
 		}	
 	};
-	request.open("GET", "test.php");
-	request.send();
-};
-
-
-function geAjax1(){
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function (){
-		if(request.readyState == 4 && request.status == 200){
-			document.querySelector(".third").innerHTML = request.responseText;
-		}	
-	};
-	request.open("GET", "stat1.php");
-	request.send();
-};
-
-function geAjax2(){
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function (){
-		if(request.readyState == 4 && request.status == 200){
-			document.querySelector(".third").innerHTML = request.responseText;
-		}	
-	};
-	request.open("GET", "stat2.php");
-	request.send();
-};
-
-function geAjax3(){
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function (){
-		if(request.readyState == 4 && request.status == 200){
-			document.querySelector(".third").innerHTML = request.responseText;
-		}	
-	};
-	request.open("GET", "stat3.php");
-	request.send();
-};
-
-function geAjax4(){
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function (){
-		if(request.readyState == 4 && request.status == 200){
-			document.querySelector(".third").innerHTML = request.responseText;
-		}	
-	};
-	request.open("GET", "stat4.php");
-	request.send();
-};
-
-function geAjax5(){
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function (){
-		if(request.readyState == 4 && request.status == 200){
-			document.querySelector(".third").innerHTML = request.responseText;
-		}	
-	};
-	request.open("GET", "stat5.php");
+	request.open("GET", res);
 	request.send();
 };
